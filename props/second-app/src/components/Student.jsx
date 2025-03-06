e30fc63263c3625c48e07f1e9c10d102ce11d17e
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2025-03-07 03:46:46 +0530

    Creating different component and fragment in react

---
+++ props/second-app/src/components/Student.jsx
@@ -0,0 +1,10 @@
+function Student (props){
+    return (
+        <>
+        <h1>your name is {props.name}</h1>
+        <h1>your age is {props.age}</h1>
+        <h1>your height is {props.height}</h1>
+        </>
+    )
+}
+export default Student;
