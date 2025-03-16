1983d00526c8b4168dfbabc60e7e31fef50371c3
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2025-03-17 04:17:23 +0530

    displaying food details by searching on search bar from API

---
+++ food-app/06-02/foodapp/src/App.jsx
@@ -0,0 +1,17 @@
+//import Food from "./components/Food"
+import { useState } from "react"
+import FoodList from "./components/FoodList"
+import Search from "./components/Search"
+
+function App() {
+  const [foodData , setFoodData] = useState([])
+
+  return (
+    <>
+      <Search setFoodData={setFoodData}/>
+      <FoodList foodData={foodData}/>
+    </>
+  )
+}
+
+export default App
