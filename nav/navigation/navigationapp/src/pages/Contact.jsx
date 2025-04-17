bc048a007e39a1ca1204f6d48e1c2a630e9c0aa5
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2025-04-17 22:28:11 +0530

    creating recipe app using react

--- nav/navigation/navigationapp/src/pages/Contact.jsx
+++
@@ -1,7 +0,0 @@
-export default function Contact(){
-    return(
-        <div>
-            <h1>contact page coming soon</h1>
-        </div>
-    )
-}
--- nav/navigation/navigationapp/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import {Link} from "react-router-dom"
-export default function Home(){
-    return(
-        <div>
-            <h1>home page coming soon...</h1>
-        </div>
-    )
-}
