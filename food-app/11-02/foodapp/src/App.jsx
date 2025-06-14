f5bb997ded51c144976e10436a03a261ecd5b8c4
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2025-06-14 23:03:09 +0530

    creating food app using react

---
+++ food-app/11-02/foodapp/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css"
+import Nav from "./components/Nav"
+import Container from "./components/Container"
+import Food from "./components/food"
+
+function App() {
+  
+
+  return (
+    <>
+      <Nav/>
+      
+      <Container>
+        <Food/>
+        <h1>hello</h1>
+      </Container>
+    </>
+  )
+}
+
+export default App
