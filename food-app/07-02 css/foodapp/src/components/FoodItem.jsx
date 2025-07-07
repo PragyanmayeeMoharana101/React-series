dfc744dc268a5da99afe8ebc20ebf30fb40b662a
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2025-07-07 23:44:39 +0530

    creating food app using react

--- food-app/07-02 css/foodapp/src/components/FoodItem.jsx
+++
@@ -1,9 +0,0 @@
-export default function FoodItem ({food}){
-    return (
-        <div className="item">
-            <h2>{food.title}</h2>
-            <h3>{food.id}</h3>
-            <img src={food.image}/>
-        </div>
-    )
-}
