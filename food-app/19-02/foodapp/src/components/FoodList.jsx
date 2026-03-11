4dba0e9c48259db7e99e73b1434652d94485eb01
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2026-03-11 23:14:34 +0530

    creating food app using react

---
+++ food-app/19-02/foodapp/src/components/FoodList.jsx
@@ -0,0 +1,11 @@
+import FoodItem from "./FoodItem"
+
+export default function FoodList ({foodData,setFoodId}){
+    return (
+        <div>
+            {foodData.map((item)=>{
+                return <FoodItem key={item.id} item={item} setFoodId={setFoodId}/>
+            })}
+        </div>
+    )
+}
